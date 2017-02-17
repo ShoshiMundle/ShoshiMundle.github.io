7862bd7ad7e76d1bf947cc4f9a304f0886bc7d12
ShoshiMundle <ksmundle@hotmail.com>
2017-02-16 21:29:36 -0500

    Class 7

---
+++ class_work/class07/first-javascript/script.js
@@ -0,0 +1,29 @@
+// alert('This sucks almost as much as a marquee');
+// the above is a datatype that is a string
+
+document.write('Writing to document');
+
+
+var firstNum = 10 * 10;
+var myName = "My name is Jonas";
+
+// thermostatReading
+
+// document.write(10*10);
+// document.write(100/10);
+// document.write(50-17);
+// document.write(50-thermostatReading);
+// document.write(50+17);
+// document.write('Shoshi Mundle');
+
+document.write(myName);
+
+function stupidAlert() {
+	alert('stupid');
+	alert('alerts are stupid');
+}
+
+document.getElementById('title').style.color = 'red';
+
+
+
